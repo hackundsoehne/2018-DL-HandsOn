352bd2a9a424939a06f6bb3e153d2cc9d20793c7
LeanderK <Leander.Kurscheidt@gmx.de>
2018-05-04 10:49:36 +0200

    reworked things

--- Basics/src/backward.py
+++ Basics/src/backward.py
@@ -1,78 +1,174 @@
 import numpy as np
 from math import fabs, fsum
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import log_loss
+from sklearn.neural_network import MLPClassifier
+from sklearn.datasets import fetch_mldata
+import matplotlib.pyplot as plt
+import numpy.random as rand
 
-class BackwardWeights:
+class BackwardWeightsBCW:
     """
-    This class provides the weights for the forward-propagation excersice
+    This class provides the weights for the forward-propagation Breast Cancer Wisconsin (Diagnostic) Dataset excersice.
+    """
+    """
+    This class provides the weights for the forward-propagation excersice.
     """
     def __init__(self):
-        self.weights = None
-        self.biases = None
+        cancer = load_breast_cancer()
+        X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, stratify=cancer.target, random_state=42)
+        self.weights = np.clip(np.random.normal(0, 1/np.sqrt(30), 30).reshape((1,30)), -2, 2)
+        self.bias = np.clip(np.random.normal(0, 1/np.sqrt(30), 1).reshape(()), -2, 2)
+        self.X_test = X_test
+        self.X_train = X_train
+        self.y_test = y_test
+        self.y_train = y_train
         
-    def updateWeights(self, layer, neuron, weights):
-        """
-        assigns new weights for an individual neuron
-        """
-        return weights[layer][neuron] = weights
+    def getTrain(self, trainFkt, forewardFktProba):
+        return Train(self.X_train, self.y_train, self.X_test, self.y_test, trainFkt, forewardFktProba, False)
     
-    def getWeights(self, layer, neuron):
-        """
-        returns the weights for an individual neuron
-        """
-        if (layer < 0 | layer > 2):
-            raise ValueError("there are only 3 layers: 2,1,0")
-        return weights[layer][neuron]
+    def setWeights(self, W):
+        self.weights = W
     
-    def getBias(self, layer, neuron):
-        """
-        returns the weights for an individual neuron
-        """
-        if (layer < 0 | layer > 2):
-            raise ValueError("there are only 3 layers: 2,1,0")
-        return biases[layer][neuron]
+    def setBias(self, b):
+        self.bias = b
     
-    def updateBias(self, layer, neuron, bias):
+    def getWeights(self):
         """
-        assigns new weights for an individual neuron
+        returns the weights
         """
-        if (layer < 0 | layer > 2):
-            raise ValueError("there are only 3 layers: 2,1,0")
-        return biases[layer][neuron] = bias
+        return self.weights
+    
+    def getBias(self):
+        """
+        returns the bias
+        """
+        return self.bias
+    
+    def resetWeights(self):
+        self.weights = np.clip(np.random.normal(0, 1/np.sqrt(30), 30).reshape((1,30)), -2, 2)
+        self.bias = np.clip(np.random.normal(0, 1/np.sqrt(30), 1).reshape(()), -2, 2)
+        
+    def eval(self,forewardPass):
+        """
+        evaluates forewardPass on the training dataset and returns the RMSE (root-mean-square error) 
+        """
+        results = forewardPass(self.X_test)
+        return mean_squared_error(self.y_test, results)
         
         
 class Train:
     """
     Evaluates the a neural network for the forward-propagation excersice.
     """
-    def __init__(self):
-        self.train_x = None
-        self.train_y = None
-        self.test_x = None
-        self.test_y = None
+    def __init__(self, train_x, train_y, test_x, test_y, trainFkt, forewardFkt, oneHot):
+        self.train_x = train_x
+        self.train_y = train_y
+        self.test_x = test_x
+        self.test_y = test_y
+        self.trainFkt = trainFkt
+        self.forewardFkt = forewardFkt
+        self.oneHot = oneHot
         
     def get_test(self):
-        return test_x
+        return self.test_x
     
-    def train(self, trainFkt, forewardFkt):
-        for (i in range (1,3000)):
-            i = rand.uniform(0, train_x.shape()[0])
-            trainFkt(train_x[i], train[i])
-            if (i % 20 == 0):
-                //TODO error
-        //TODO total error
+    def train(self, iterations, eval_):
+        #plt.figure()
+        #plt.title("training")
+        for i in range(1,iterations):
+            if (i % eval_ == 0 or i == 0):
+                loss_test = 0
+                loss_train = 0
+                for x in range (1,5):
+                    ind = np.random.choice(self.test_x.shape[0], 1)
+                    y_test = self.test_y[ind]
+                    if (self.oneHot):
+                        y_test = np.zeros((1, 10))
+                        y_test[self.test_y[ind]] = 1
+                    res_test = self.forewardFkt(self.test_x[ind]).reshape((1,))
+                    loss_test += log_loss(y_test, res_test, labels=[0,1])
+                    
+                    ind = np.random.choice(self.train_x.shape[0], 1)
+                    y_train = self.train_y[ind]
+                    if (self.oneHot):
+                        y_train = np.zeros((1, 10))
+                        y_train[self.train_x[ind]] = 1
+                    #import pdb; pdb.set_trace()
+                    res_train = self.forewardFkt(self.train_x[ind]).reshape((1,))
+                    loss_train += log_loss(y_train, res_train, labels=[0,1])
+                print ("train", loss_train / 5.)
+                print ("test", loss_test / 5.)
+            ind = np.random.choice(self.train_x.shape[0], 1)
+            self.trainFkt(self.train_x[ind], self.train_y[ind])
+
+        #TODO total error
     
-    def eval(self, results):
-        """
-        Returns the mean absolute error (MAE) of the neural network for the prediction.
-        """
-        losses = [fabs(ni - yi) for ni in results for yi in test_y]
-        return fsum(losses)/length(losses)
-    
-class TrainHelper:
+class BackwardWeightsMNISt:
     """
-    TrainHelper for the assignments
+    This class provides the weights for the forward-propagation Breast Cancer Wisconsin (Diagnostic) Dataset excersice.
+    """
+    """
+    This class provides the weights for the forward-propagation excersice.
     """
     def __init__(self):
-        sef.train = new Train()
+        mnist = fetch_mldata("MNIST original")
+        # rescale the data, use the traditional train/test split
+        X, y = mnist.data / 255., mnist.target
+        X_train, X_test = X[:60000], X[60000:]
+        y_train, y_test = y[:60000], y[60000:]
+        self.weights0 = np.clip(np.random.normal(0, 1/np.sqrt(30), 39200).reshape((784, 50)), -2, 2)
+        self.weights1 = np.clip(np.random.normal(0, 1/np.sqrt(30), 500).reshape((50,10)), -2, 2)
+        self.bias0 = np.clip(np.random.normal(0, 1/np.sqrt(30), 50).reshape((50,)), -2, 2)
+        self.bias1 = np.clip(np.random.normal(0, 1/np.sqrt(30), 10).reshape((10,)), -2, 2)
+        self.X_test = X_test
+        self.X_train = X_train
+        self.y_test = y_test
+        self.y_train = y_train
         
-    def 
+    def getTrain(self, trainFkt, forewardFktProba):
+        return Train(self.X_train, self.y_train, self.X_test, self.y_test, trainFkt, forewardFktProba, False)
+    
+    def setWeights(self, W):
+        self.weights = W
+    
+    def setBias(self, b):
+        self.bias = b
+    
+    def getWeights(self, layer):
+        """
+        returns the weights
+        """
+        if (layer < 0 | layer > 1):
+            raise ValueError("there are only 2 layers: 1,0")
+        if (layer == 0):
+            return self.weights0
+        else:
+            return self.weights1
+    
+    def getBias(self, layer):
+        """
+        returns the bias
+        """
+        if (layer < 0 | layer > 1):
+            raise ValueError("there are only 2 layers: 1,0")
+        if (layer == 0):
+            return self.bias0
+        else:
+            return self.bias1
+    
+    def resetWeights(self):
+        self.weights0 = np.clip(np.random.normal(0, 1/np.sqrt(30), 39200).reshape((784, 50)), -2, 2)
+        self.weights1 = np.clip(np.random.normal(0, 1/np.sqrt(30), 500).reshape((50,10)), -2, 2)
+        self.bias0 = np.clip(np.random.normal(0, 1/np.sqrt(30), 50).reshape((50,)), -2, 2)
+        self.bias1 = np.clip(np.random.normal(0, 1/np.sqrt(30), 10).reshape((10,)), -2, 2)
+        
+    def eval(self,forewardPass):
+        """
+        evaluates forewardPass on the training dataset and returns the RMSE (root-mean-square error) 
+        """
+        results = forewardPass(self.X_test)
+        return mean_squared_error(self.y_test, results)
--- Basics/src/forward.py
+++ Basics/src/forward.py
@@ -1,45 +1,119 @@
 import numpy as np
 from math import fabs, fsum
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.neural_network import MLPClassifier
+from sklearn.datasets import fetch_mldata
+import matplotlib.pyplot as plt
 
-class ForewardWeights:
+class ForewardWeightsBCW:
+    """
+    This class provides the weights for the forward-propagation Breast Cancer Wisconsin (Diagnostic) Dataset excersice.
+    """
     """
     This class provides the weights for the forward-propagation excersice.
     """
     def __init__(self):
-        self.weights = None
-        self.biases = None
+        cancer = load_breast_cancer()
+        X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, stratify=cancer.target, random_state=42)
+        log_reg = LogisticRegression()
+        log_reg.fit(X_train, y_train)
+        self.weights = log_reg.coef_
+        self.bias = log_reg.intercept_[0]
+        self.X_test = X_test
+        self.y_test = y_test
     
-    def getWeights(self, layer, neuron):
+    def getWeights(self):
         """
-        returns the weights for an individual neuron
+        returns the weights
         """
-        if (layer < 0 | layer > 2):
-            raise ValueError("there are only 3 layers: 2,1,0")
-        return weights[layer][neuron]
+        return self.weights
     
-    def getBias(self, layer, neuron):
+    def getBias(self):
         """
-        returns the weights for an individual neuron
+        returns the bias
         """
-        if (layer < 0 | layer > 2):
-            raise ValueError("there are only 3 layers: 2,1,0")
-        return biases[layer][neuron]
+        return self.bias
         
+    def eval(self,forewardPass):
+        """
+        evaluates forewardPass on the training dataset and returns the RMSE (root-mean-square error) 
+        """
+        results = forewardPass(self.X_test)
+        return mean_squared_error(self.y_test, results)
         
-class Evaluation:
+
+class ForewardWeightsMNIST:
     """
-    Evaluates the a neural network for the forward-propagation excersice.
+    This class provides the weights for the forward-propagation excersice.
     """
     def __init__(self):
-        self.test_x = None
-        self.test_y = None
-        
-    def get_test(self):
-        return test_x
+        mnist = fetch_mldata("MNIST original")
+        # rescale the data, use the traditional train/test split
+        X, y = mnist.data / 255., mnist.target
+        X_train, X_test = X[:60000], X[60000:]
+        y_train, y_test = y[:60000], y[60000:]
+
+        mlp = MLPClassifier(activation='logistic', hidden_layer_sizes=(50,), max_iter=20, alpha=1e-4,
+                            solver='sgd', tol=1e-4, random_state=1,
+                            learning_rate_init=.1)
+
+        mlp.fit(X_train, y_train)
+        self.mlp=mlp
+        self.weights = mlp.coefs_
+        self.biases = mlp.intercepts_
+        self.X_test = X_test
+        self.y_test = y_test
     
-    def eval(self, results):
+    def getWeights(self, layer):
         """
-        Returns the mean absolute error (MAE) of the neural network for the prediction.
+        returns the weights for the layer
         """
-        losses = [fabs(ni - yi) for ni in results for yi in test_y]
-        return fsum(losses)/length(losses)
+        if (layer < 0 | layer > 1):
+            raise ValueError("there are only 2 layers: 1,0")
+        return self.weights[layer]
+    
+    def getBias(self, layer):
+        """
+        returns the biases for the layer
+        """
+        if (layer < 0 | layer > 1):
+            raise ValueError("there are only 2 layers: 1,0")
+        return self.biases[layer]
+    
+    def getRandom(self):
+        idx = np.random.randint(self.y_test.shape[0], size=2)
+        r_x = self.X_test[idx,:]
+        r_y = self.y_test[idx]
+        return (r_x, r_y)
+    
+    def visualize(self,forewardPassPropa):
+        (r_x,r_y) = self.getRandom()
+        pred = forewardPassPropa(r_x)
+
+        plt.figure(figsize=(20,4))
+        plt.subplot(1, 5, 1)
+        plt.imshow(np.reshape(r_x[0], (28,28)), cmap=plt.cm.gray)
+        plt.title('Label: %i\n' % r_y[0], fontsize = 20)
+        
+        plt.subplot(1, 5, 2)
+        plt.bar(np.arange(0,10,1), pred[0], align='center', alpha=0.5)
+        plt.title('Prediction\n', fontsize = 20)
+
+        plt.subplot(1, 5, 3)
+        plt.imshow(np.reshape(r_x[1], (28,28)), cmap=plt.cm.gray)
+        plt.title('Label: %i\n' % r_y[1], fontsize = 20)
+        
+        plt.subplot(1, 5, 4)
+        plt.bar(np.arange(0,10,1), pred[1])
+        plt.title('Prediction\n', fontsize = 20)
+    
+    def eval(self,forewardPassClass):
+        """
+        evaluates forewardPass on the training dataset and returns the RMSE (root-mean-square error) 
+        """
+        print("mse real:",mean_squared_error(self.y_test, self.mlp.predict(self.X_test)))
+        results = forewardPassClass(self.X_test)
+        return mean_squared_error(self.y_test, results)
