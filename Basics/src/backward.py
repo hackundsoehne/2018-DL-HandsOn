9cf9b87a76f49ce1b11c688d7c02c8c6a9b7bc94
LeanderK <Leander.Kurscheidt@gmx.de>
2018-05-30 14:54:38 +0200

    progress on the assignments

--- Basics/src/backward.py
+++ Basics/src/backward.py
@@ -27,8 +27,15 @@ class BackwardWeightsBCW:
         self.y_test = y_test
         self.y_train = y_train
         
+    def getTrainID(self, trainFkt, forewardFktProba):
+        X_ID_test = np.ones(self.X_test.shape)
+        X_ID_test[:,0] = self.y_test
+        X_ID_train = np.ones(self.X_train.shape)
+        X_ID_train[:,0] = self.y_train
+        return Train(X_ID_train, self.y_train, X_ID_test, self.y_test, trainFkt, forewardFktProba, False, False)
+        
     def getTrain(self, trainFkt, forewardFktProba):
-        return Train(self.X_train, self.y_train, self.X_test, self.y_test, trainFkt, forewardFktProba, False)
+        return Train(self.X_train, self.y_train, self.X_test, self.y_test, trainFkt, forewardFktProba, False, False)
     
     def setWeights(self, W):
         self.weights = W
@@ -64,7 +71,7 @@ class Train:
     """
     Evaluates the a neural network for the forward-propagation excersice.
     """
-    def __init__(self, train_x, train_y, test_x, test_y, trainFkt, forewardFkt, oneHot):
+    def __init__(self, train_x, train_y, test_x, test_y, trainFkt, forewardFkt, oneHot, batches):
         self.train_x = train_x
         self.train_y = train_y
         self.test_x = test_x
@@ -72,18 +79,25 @@ class Train:
         self.trainFkt = trainFkt
         self.forewardFkt = forewardFkt
         self.oneHot = oneHot
+        self.batches = batches
+        
+    def shuffle(self):
+        
         
     def get_test(self):
         return self.test_x
     
     def train(self, iterations, eval_):
+        
         #plt.figure()
         #plt.title("training")
-        for i in range(1,iterations):
+        train_loss = np.array([])
+        test_loss = np.array([])
+        for i in range(0,iterations):
             if (i % eval_ == 0 or i == 0):
                 loss_test = 0
                 loss_train = 0
-                for x in range (1,5):
+                for x in range (1,10):
                     ind = np.random.choice(self.test_x.shape[0], 1)
                     y_test = self.test_y[ind]
                     if (self.oneHot):
@@ -100,13 +114,25 @@ class Train:
                     #import pdb; pdb.set_trace()
                     res_train = self.forewardFkt(self.train_x[ind]).reshape((1,))
                     loss_train += log_loss(y_train, res_train, labels=[0,1])
-                print ("train", loss_train / 5.)
-                print ("test", loss_test / 5.)
+                train_loss = np.append(train_loss, loss_train / 10.)
+                test_loss = np.append(test_loss, loss_test / 10.)
+                #print ("train", loss_train / 5.)
+                #print ("test", loss_test / 5.)
             ind = np.random.choice(self.train_x.shape[0], 1)
             self.trainFkt(self.train_x[ind], self.train_y[ind])
 
         #TODO total error
-    
+        plt.figure()
+        plt.title("Test & Training cross-entropy loss")
+        plt.xlabel("Training examples")
+        plt.ylabel("Score")
+        plt.grid()
+        it = np.arange(0., test_loss.shape[0], 1)
+        plt.plot(it, train_loss, 'o-', color="r",
+             label="Training score")
+        plt.plot(it, test_loss, 'o-', color="g",
+                 label="Test score")
+        plt.legend(loc="best")
 class BackwardWeightsMNIST:
     """
     This class provides the weights for the forward-propagation Breast Cancer Wisconsin (Diagnostic) Dataset excersice.
